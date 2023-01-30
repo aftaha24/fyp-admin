ee41df248b7faad5e5d03be13d2e3dd0bc38465e
banku27 <pankajparihar123321@gmail.com>
2023-01-31 01:30:11 +0530

    fixes

--- lib/presentation/widgets/custom_textfield.dart
+++ lib/presentation/widgets/custom_textfield.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 import 'package:traceebee_admin_app/utlis/text_styles.dart';
 
 class CustomTextfield extends StatelessWidget {
-  const CustomTextfield({super.key, this.hintText, this.controller});
+  CustomTextfield({
+    Key? key,
+    this.hintText,
+    this.isObscure = false,
+    this.controller,
+  }) : super(key: key);
   final String? hintText;
+  bool isObscure;
+
   final TextEditingController? controller;
   @override
   Widget build(BuildContext context) {
@@ -14,6 +22,7 @@ class CustomTextfield extends StatelessWidget {
       child: TextFormField(
         controller: controller,
         style: subHeadingStyle,
+        obscureText: isObscure,
         decoration: InputDecoration(
           contentPadding: EdgeInsets.symmetric(horizontal: 20.sp),
           fillColor: Colors.white,
